c985f4bb906a4dafc982a8a89c4d05db3a68e272
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-14 18:24:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,12 +18,14 @@ class Scraper
   end
   
   def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    
+        binding.pry
   end
   
   
 end
 
+Scraper.new.get_page
 
 
